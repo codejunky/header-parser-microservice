bec117675911c2159685eb5755d0d996e973a79e
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-22 21:38:10 +0100

    extract OS from header and add it to the json response

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ app.get('*', function (req, res) {
   var header = req.headers
   var language = header['accept-language'].split(',')[0]
   var ip = header['x-forwarded-for'] || req.connection.remoteAddress
+  var ua = header['user-agent'];
+  var os = ua.substring(ua.indexOf('(')+1, ua.indexOf(')'));
+
+  console.log(os);
 
   res.json({
     "ip": ip,
     "language": language,
-    "operating system": ""
+    "operating system": os
   })
 });
 
 
 
-app.listen(process.env.PORT || 3000, function () {console.log('running')} )
-//I can get the IP address, language and operating system for my browser.
+app.listen(process.env.PORT || 3000, function () {console.log('running')} );
